a8a5e2551d53b7b37a8e449140ccdca4dbb6bcb2
Carter Shadden <carter.shadden@gmail.com>
2019-12-31 11:17:55 -0500

    updated

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -8,105 +8,105 @@ class Search extends Component {
             {
                 id: 1,
                 name: "Apple",
-                img: [ process.env.PUBLIC_URL + '/images/apple.jpg'],
+                img: ['/images/apple.jpg'],
                 caption: "Keeps the doctor away!",
                 category: "fruit"
             },
             {
                 id: 2,
                 name: "Paint brush",
-                img: [ process.env.PUBLIC_URL + '/images/paintbrush.jpg'],
+                img: ['/images/paintbrush.jpg'],
                 caption: "Paint not included.",
                 category: "art"
             },
             {
                 id: 3,
                 name: "Cup of coffee",
-                img: [ process.env.PUBLIC_URL + '/images/hotCoffee.jpg'],
+                img: ['/images/hotCoffee.jpg'],
                 caption: "Piping hot and full of caffeine!",
                 category: "drink"
             },
             {
                 id: 4,
                 name: "Iced coffee",
-                img: [ process.env.PUBLIC_URL + '/images/icedCoffee.jpg'],
+                img: ['/images/icedCoffee.jpg'],
                 caption: "Pairs well with our hot coffee.",
                 category: "drink"
             },
             {
                 id: 5,
                 name: "Red rose",
-                img: [ process.env.PUBLIC_URL + '/images/redRose.jpeg'],
+                img: ['/images/redRose.jpeg'],
                 caption: "Smells good!",
                 category: "flower"
             },
             {
                 id: 6,
                 name: "Sunflowers",
-                img: [ process.env.PUBLIC_URL + '/images/sunflower.jpg'],
+                img: ['/images/sunflower.jpg'],
                 caption: "For your garden or kitchen.",
                 category: "flower"
             },
             {
                 id: 7,
                 name: "Naked Rat",
-                img: [process.env.PUBLIC_URL + '/images/nakedRat.jpg'],
+                img: ['/images/nakedRat.jpg'],
                 caption: "Hairless like a sphynx cat, affectionate like a pup - pairs well with Albino Rat.",
                 category: "animal"
             },
             {
                 id: 8,
                 name: "Albino Rat",
-                img: [process.env.PUBLIC_URL + '/images/albinoRat.jpg'],
+                img: ['/images/albinoRat.jpg'],
                 caption: "Affectionate and hyperactive, this widdle wat pairs well with the Naked Rat.",
                 category: "animal"
             },
             {
                 id: 9,
                 name: "Cactus",
-                img: [process.env.PUBLIC_URL + '/images/cactus.jpg'],
+                img: ['/images/cactus.jpg'],
                 caption: "Prickly and easy to grow!",
                 category: ""
             },
             {
                 id: 10,
                 name: "Yellow Mushroom",
-                img: [process.env.PUBLIC_URL + '/images/mushroom.jpg'],
+                img: ['/images/mushroom.jpg'],
                 caption: "Fungus among us...",
                 category: "fungus"
             },
             {
                 id: 11,
                 name: "Red Mushroom",
-                img: [process.env.PUBLIC_URL + '/images/redMushroom.jpg'],
+                img: ['/images/redMushroom.jpg'],
                 caption: "Fungus among us, now in red!",
                 category: "fungus"
             },
             {
                 id: 12,
                 name: "Gecko",
-                img: [process.env.PUBLIC_URL + '/images/dancingGecko.jpg'],
+                img: ['/images/dancingGecko.jpg'],
                 caption: "A cute reptile that sticks to walls!",
                 category: "animal"
             },
             {
                 id: 13,
                 name: "White Rose",
-                img: [process.env.PUBLIC_URL + '/images/whiteRose.jpg'],
+                img: ['/images/whiteRose.jpg'],
                 caption: "Smells good, comes in white.",
                 category: "flower"
             },
             {
                 id: 14,
                 name: "Latte Art",
-                img: [process.env.PUBLIC_URL + '/images/latteArt.jpg'],
+                img: ['/images/latteArt.jpg'],
                 caption: "Makes any coffee look that much fancier!",
                 category: "drink"
             },
             {
                 id: 15,
                 name: "Fused Bananas",
-                img: [process.env.PUBLIC_URL + '/images/fusedBanana.jpg'],
+                img: ['/images/fusedBanana.jpg'],
                 caption: "Is it one banana or two? You decide!",
                 category: "food"
             }
